8ad1ce0adeea2e41b79a916c6f0a199ec27bbf5c
Marie Desaulniers <desm137@uqo.ca>
2016-02-23 23:00:37 -0500

    Modification à l'interface et code commenté. Fonctionne dans tous les navigateurs

--- Interface/etat.js
+++ Interface/etat.js
@@ -1,39 +1,42 @@
-var etat = {"inbox": [{"from": "AF22111212232211122","date": "2015 12 28 20:15:42","msg": "Un court message ...." },{"from": "AF22111212232211122","date": "2016 01 03 10:15:31","msg": "Un autre message ...." }],
-"outbox":[{"to": "AF22111212232211122","date": "2016 01 12 20:15:42","msg": "Bla bla bla ...." }],
-"yp": {"AF22111212232211122": {"name": "Jean Fanchon"},"90221F212A4200001AA": {"name": "Bob"}}};
+var etat = {"inbox": [{"from": "AF22111212232211122","date": "2015 12 28 20:15:42","msg": "On a une réunion demain à 13h00" },{"from": "AF22111212232211122","date": "2016 01 03 10:15:31","msg": "On se rencontre à 12h00 pour le lunch" }],
+"outbox":[{"to": "AF22111212232211122","date": "2016 01 12 20:15:42","msg": "Viens-tu au cours ce soir?" }],
+"yp": {"AF22111212232211122": {"name": "Jean Fanchon"},"90221F212A4200001AA": {"name": "Robert Pratt"},"BA2674F84578427C784": {"name": "Jemal Djari"}}};
 
-
+//Fonction qui affiche la liste des messages reçus dans la fenêtre 'cible'
 function afficheInbox()
 {
 	var messages = "";
 	for(var i=0;i<etat.inbox.length;i++) {
-		 console.log("Recu de: " + etat.inbox[i].from + "\nLe: " + etat.inbox[i].date + "\nMessage: " + etat.inbox[i].msg);
-		  messages  = messages + "Recu de: " + etat.inbox[i].from + "\nLe: " + etat.inbox[i].date + "\nMessage: " + etat.inbox[i].msg + "<br>"
-	 };
-	 document.getElementById('Cible').innerHTML = messages;
+		  messages  = messages + "Recu de: " + etat.inbox[i].from + " Le: " + etat.inbox[i].date + "<br>Message: " + etat.inbox[i].msg + "<br><br>"
+	};
+	document.getElementById('TitreCible').innerHTML = "Boite de réception";
+	document.getElementById('Cible').innerHTML = messages;
  }
 
+//Fonction qui affiche la liste des messages envoyés dans la fenêtre 'cible'
 function afficheOutbox()
 {
 	var envoies = "";
 	 for(var i=0;i<etat.outbox.length;i++) {
-		 console.log("Envoyé à : " + etat.outbox[i].to + "\nLe: " + etat.outbox[i].date + "\nMessage: " + etat.outbox[i].msg);
-		 envoies = envoies + "Envoyé à"+ etat.outbox[i].to + "\nLe: " + etat.outbox[i].date + "\nMessage: " + etat.outbox[i].msg + "<br>"
+		 envoies = envoies + "Envoyé à: "+ etat.outbox[i].to + " Le: " + etat.outbox[i].date + "<br>Message: " + etat.outbox[i].msg + "<br><br>"
 	 };
+	 document.getElementById('TitreCible').innerHTML = "Messages envoyés";
 	 document.getElementById('Cible').innerHTML = envoies;
 }
  
+//Fonction qui affiche la liste des contacts dans la fenêtre 'cible'
 function afficheYp()
 {
 	var contacts = "";
 	var cles = Object.keys(etat.yp);
 	for(var i=0;i<cles.length;i++){
-		console.log("Adresse: " + cles[i] + "\nNom: " + etat.yp[cles[i]].name);
-		contacts = contacts + "Adresse: " + cles[i] + "\nNom: " + etat.yp[cles[i]].name + "<br>"
-  };
+		contacts = contacts + "Adresse: " + cles[i] + "<br>Nom: " + etat.yp[cles[i]].name + "<br><br>"
+	};
+	document.getElementById('TitreCible').innerHTML = "Liste des contacts";
 	document.getElementById('Cible').innerHTML = contacts;
 }
 
+//Fonction qui ajoute le nouveau contact dans la variable etat.yp
 function ajouterContact(form){
 	var adresse=form.inputadresse.value;
 	var nom=form.inputnom.value;
@@ -42,6 +45,7 @@ function ajouterContact(form){
 	etat.yp[adresse] = objNom;
 }
 
+//Fonction qui ajoute le nouveau message envoyés dans la variable etat.outbox
 function ajouterMessage(form){
 	var adresse=form.toAdresse.value;
 	//var date=Date.now();
